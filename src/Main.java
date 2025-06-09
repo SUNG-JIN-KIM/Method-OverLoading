6c7e133d48a3ba2dfbd11e3fc41fb297ac5f6720
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-09 10:36:21 +0900

    Method Overloading Commit_2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
 public class Main {
-    public static int add(int a, int b){
-        return a + b;
+    /*public static void greet() {
+        System.out.println("Hello, World!");
     }
 
-    public static int add(int a, int b, int c){
-        return a + b + c;
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }*/
+
+    public static String greet() {
+        return "Hello, World!";
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
     }
 
     public static void main(String[] args) {
-        System.out.println("a + b = " + add(1,2));
 
-        System.out.println("a + b + c = " + add(1,2,3));
+        System.out.println(greet());
+        System.out.println(greet("Alice"));
     }
 }
