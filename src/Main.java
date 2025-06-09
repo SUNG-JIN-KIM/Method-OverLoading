6caf5127aba9121309f3d2162b8e3706c844371b
SUNG-JIN-KIM <ksjcloud98@gmail.com>
2025-06-09 10:15:09 +0900

    Method Overloading Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("a + b = " + add(1,2));
+
+        System.out.println("a + b + c = " + add(1,2,3));
+    }
+}
